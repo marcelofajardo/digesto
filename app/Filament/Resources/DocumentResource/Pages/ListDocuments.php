44f5f139aada522bde74cd52611daf74abde99b1
marcelofajardo <marcelo.fajardo@gmail.com>
2025-06-27 20:16:07 -0300

    tags final

--- app/Filament/Resources/DocumentResource/Pages/ListDocuments.php
+++ app/Filament/Resources/DocumentResource/Pages/ListDocuments.php
@@ -5,7 +5,7 @@ namespace App\Filament\Resources\DocumentResource\Pages;
 use App\Filament\Resources\DocumentResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
-use Filament\Forms\Components\Select;
+
 
 use Filament\Forms\Components\Actions\Action;
 class ListDocuments extends ListRecords
@@ -18,18 +18,18 @@ class ListDocuments extends ListRecords
             Actions\CreateAction::make()->label('Nuevo Documento')
                 ->icon('heroicon-o-plus')
                 ->color('primary'),
-            Actions\Select::make('anio')
+            /* Actions\Select::make('anio')
                 ->label('Año')
                 /* ->options(
                     Customer::all()->pluck('name', 'id') // Opciones iniciales (si las hay)
                 ) */
-                ->getSearchResultsUsing(function (string $search) {
+                /* ->getSearchResultsUsing(function (string $search) {
                     return Document::where('anio', 'like', "%{$search}%")
                         //->orWhere('phone', 'like', "%{$search}%")
                         //->orWhere('name', 'like', "%{$search}%")
                         ->limit(50) // Limitar resultados
                         ->pluck('anio', 'id');
-                }),
+                }), */
         ];
     }
     public function getTitle(): string
